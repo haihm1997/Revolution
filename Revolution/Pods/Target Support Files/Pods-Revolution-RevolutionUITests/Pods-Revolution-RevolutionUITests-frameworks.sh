c320dda20dc8111bc6733c9b00636b796b8cf9bf
Hoàng Hải <hai.hm@teko.vn>
2021-07-20 22:54:09 +0700

    Editable image

--- Revolution/Pods/Target Support Files/Pods-Revolution-RevolutionUITests/Pods-Revolution-RevolutionUITests-frameworks.sh
+++ Revolution/Pods/Target Support Files/Pods-Revolution-RevolutionUITests/Pods-Revolution-RevolutionUITests-frameworks.sh
@@ -181,12 +181,16 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSObject+Rx/NSObject_Rx.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PryntTrimmerView/PryntTrimmerView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SteviaLayout/Stevia.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YPImagePicker/YPImagePicker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ZLImageEditor/ZLImageEditor.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
@@ -195,12 +199,16 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSObject+Rx/NSObject_Rx.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PryntTrimmerView/PryntTrimmerView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SteviaLayout/Stevia.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YPImagePicker/YPImagePicker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ZLImageEditor/ZLImageEditor.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Revolution/Revolution/Modules/Home/Views/HomeViewController.swift
+++ Revolution/Revolution/Modules/Home/Views/HomeViewController.swift
@@ -7,12 +7,91 @@
 //
 
 import UIKit
+import SnapKit
+import RxCocoa
+import YPImagePicker
+import ZLImageEditor
 
 class HomeViewController: BaseViewController {
     
+    let openPickerButton = configure(UIButton()) {
+        $0.setTitle("Mở Picker", for: .normal)
+        $0.setTitleColor(.blue, for: .normal)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view.addSubviews(openPickerButton)
+        openPickerButton.snp.makeConstraints { maker in
+            maker.centerX.centerY.equalToSuperview()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .white
+        bind()
+    }
+    
+    private func bind() {
+        openPickerButton.rx.tap.bind(to: openPickerBinder).disposed(by: rx.disposeBag)
+    }
+    
+}
+
+extension HomeViewController {
+    
+    var openPickerBinder: Binder<Void> {
+        return Binder(self) { targer, _ in
+            let config = targer.configCamera()
+            let picker = YPImagePicker(configuration: config)
+            picker.didFinishPicking { items, isSuccess in
+                guard let modifiedImage = items.singlePhoto?.image else { return }
+                picker.dismiss(animated: true) {
+                    ZLImageEditorConfiguration.default().editImageTools = [.draw, .clip, .imageSticker, .textSticker, .mosaic, .filter]
+                    ZLEditImageViewController.showEditImageVC(parentVC: self, image: modifiedImage, editModel: nil) { [weak self] (resImage, editModel) in
+                        print("image modified!")
+                    }
+                }
+            }
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    private func configCamera() -> YPImagePickerConfiguration {
+        var config = YPImagePickerConfiguration()
+        config.isScrollToChangeModesEnabled = true
+        config.onlySquareImagesFromCamera = true
+        config.usesFrontCamera = false
+        config.showsPhotoFilters = true
+        config.showsVideoTrimmer = true
+        config.shouldSaveNewPicturesToAlbum = true
+        config.albumName = "DefaultYPImagePickerAlbumName"
+        config.startOnScreen = YPPickerScreen.photo
+        config.screens = [.library, .photo]
+        config.showsCrop = .none
+        config.targetImageSize = YPImageSize.original
+        config.overlayView = UIView()
+        config.hidesStatusBar = true
+        config.hidesBottomBar = false
+        config.hidesCancelButton = false
+        config.preferredStatusBarStyle = UIStatusBarStyle.default
+        config.maxCameraZoomFactor = 1.0
+        
+        config.library.options = nil
+        config.library.onlySquare = false
+        config.library.isSquareByDefault = true
+        config.library.minWidthForItem = nil
+        config.library.mediaType = YPlibraryMediaType.photo
+        config.library.defaultMultipleSelection = false
+        config.library.maxNumberOfItems = 1
+        config.library.minNumberOfItems = 1
+        config.library.numberOfItemsInRow = 4
+        config.library.spacingBetweenItems = 1.0
+        config.library.skipSelectionsGallery = false
+        config.library.preselectedItems = nil
+    
+        return config
     }
     
 }
