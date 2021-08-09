1f46a19f271ea764f153f930ff4cd29517d51cac
Hoang Hai <haihm@techcombank.com.vn>
2021-08-09 23:52:41 +0700

    add fonts

---
+++ Revolution/Revolution/Modules/InsertText/ViewModel/StickerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  StickerViewModel.swift
+//  Revolution
+//
+//  Created by Hai IT. Hoang Minh on 09/08/2021.
+//  Copyright © 2021 TonyHoang. All rights reserved.
+//
+
+import Foundation
---
+++ Revolution/Revolution/Modules/InsertText/Views/StickerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StickerViewController.swift
+//  Revolution
+//
+//  Created by Hai IT. Hoang Minh on 09/08/2021.
+//  Copyright © 2021 TonyHoang. All rights reserved.
+//
+
+import Foundation
